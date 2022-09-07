a18affefe513b6247fded4e6e050fad07d2bea31
AgFe2 <103208760+AgFe2@users.noreply.github.com>
2022-09-07 20:30:35 +0900

    Add files via upload

---
+++ src/main/java/B4F2/PetStagram/PetStagramApplication.java
@@ -0,0 +1,13 @@
+package B4F2.PetStagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStagramApplication.class, args);
+	}
+
+}
