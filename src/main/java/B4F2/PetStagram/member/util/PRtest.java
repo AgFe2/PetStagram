b5c91885add650b957dca5af89e2ec47fcd6a94a
DYS <kcood92@gmail.com>
2022-09-14 12:11:20 +0900

    PR Test

--- src/main/java/B4F2/PetStagram/member/util/PRtest.java
+++ src/main/java/B4F2/PetStagram/member/util/PRtest.java
@@ -1,7 +1,7 @@
 package B4F2.PetStagram.member.util;
 
-public class PrTest {
+public class PRtest {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("hello");
     }
 }
