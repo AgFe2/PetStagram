24bb12ee6200a9f60c481197b7b65162eb9a1a03
chaeaw04 <chaeaw04@gmail.com>
2022-09-15 18:51:02 +0900

    page 뼈대 및 css 작성

---
+++ front/src/components/Contents.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/etcContent.css";
+
+function Contents(props) {
+  return (
+    <>
+      <div className="top-contents-item">
+        <img src="" alt="" className="item-img"></img>
+        <div className="item-back"></div>
+      </div>
+    </>
+  );
+}
+
+export default Contents;
