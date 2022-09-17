3f6b05ebb36dffb428e5a59777e2d1fabe4edaff
DYS <kcood92@gmail.com>
2022-09-17 14:10:32 +0900

    Changes 리팩토링 전 현재 상태 저장

--- src/main/java/B4F2/PetStagram/configuration/SecurityConfiguration.java
+++ src/main/java/B4F2/PetStagram/configuration/SecurityConfiguration.java
@@ -8,56 +8,95 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration  {
 
-    @Bean
-    MemberAuthenticationFailureHandler getFailureHandler(){
-        return new MemberAuthenticationFailureHandler();
-    }
+//    private final MemberFilter memberFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-
-        return http.csrf().disable()
+        return http
+                .cors().disable()
+                .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
 
                 .authorizeRequests()
-                //todo 전체 접근 허용 경로
                 .antMatchers(
-                        "/"
+                        // todo 모든 접근 허용 경로
+                        "/*"
 //                        ,
 //                        "/**"
 //                        ,
-//                        "/member/**"
-                        )
-                .permitAll()
-                .and()
+//                        "/member/*"
+                ).permitAll()
 
-//                todo 프론트 협업시 로그인 실패 이 방식 사용?
-//                .formLogin()
-//                .loginPage("/login")
-//                .failureHandler(getFailureHandler())
-//                .permitAll()
-//                .and()
-
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-                .and()
-
-//                .antMatchers(HttpMethod.GET, "/**")
-//                .permitAll()
 //                .anyRequest().authenticated()
-//                .and()
+
+                .and()
 
                 .build();
     }
+//
+//        return http
+//
+//                //todo custom필터 적용...
+////                .addFilterAfter(this.memberFilter, UsernamePasswordAuthenticationFilter.class)
+//
+//                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//
+//                .authorizeRequests()
+//                //todo 전체 접근 허용 경로
+//                .antMatchers(
+//                        "/*"
+////                        ,
+////                        "/*"
+////                                ,
+////                                "/member/**"
+//                )
+//                .permitAll()
+//                .and()
+//
+//                //todo custom필터 적용...
+////                .addFilterBefore(this.memberFilter, UsernamePasswordAuthenticationFilter.class)
+//
+//                // todo 요놈도 문제 -> MemberFilter를 customFilter 적용 후 authenticated사용해야될듯
+////                .authenticated() //적용시 403에러 정상적으로 뜨지만, 로그인도 토큰들고와도 403띄움
+//
+////             .permitAll() //적용시 403에러 안뜨지만, 로그인 정상작동
+//
+//
+////                .and()
+//
+////                todo 프론트 협업시 로그인 실패 이 방식 사용
+////                .formLogin()
+////                .loginPage("/login")
+////                .failureHandler(getFailureHandler())
+////                .permitAll()
+////                .and()
+//
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+//                .logoutSuccessUrl("/")
+//                .invalidateHttpSession(true)
+//                .and()
+//
+////                .antMatchers(HttpMethod.GET, "/**")
+////                .permitAll()
+////                .anyRequest().authenticated()
+////                .and()
+//
+//                .build();
+//    }
+
+
+
+
 }
--- src/main/java/B4F2/PetStagram/configuration/filter/MemberFilter.java
+++ src/main/java/B4F2/PetStagram/configuration/filter/MemberFilter.java
@@ -4,6 +4,7 @@ import B4F2.PetStagram.configuration.JwtAuthenticationProvider;
 import B4F2.PetStagram.member.domain.MemberVo;
 import B4F2.PetStagram.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -11,24 +12,25 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 // todo 로그인유저만 접근 가능 경로 설정
-@WebFilter(urlPatterns = "/member/sign-in")
-@RequiredArgsConstructor
-public class MemberFilter implements Filter {
-    private final JwtAuthenticationProvider jwtAuthenticationProvider;
-    private final MemberService memberService;
-
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) request;
-        String token = req.getHeader("X-AUTH-TOKEN");
-        if(!jwtAuthenticationProvider.validateToken(token)){
-            throw new ServletException("Invalid Access");
-        }
-        MemberVo vo = jwtAuthenticationProvider.getMemberVo(token);
-        memberService.findByIdAndEmail(vo.getId(), vo.getEmail()).orElseThrow(
-                ()->new ServletException("Invalid access")
-        );
-        chain.doFilter(request,response);
-    }
-}
+//@WebFilter(urlPatterns = "/customercustomer/sign-in")
+//@RequiredArgsConstructor
+//@Component
+//public class MemberFilter implements Filter {
+//    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+//    private final MemberService memberService;
+//
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        HttpServletRequest req = (HttpServletRequest) request;
+//        String token = req.getHeader("X-AUTH-TOKEN");
+//        if(!jwtAuthenticationProvider.validateToken(token)){
+//            throw new ServletException("Invalid Access 1");
+//        }
+//        MemberVo vo = jwtAuthenticationProvider.getMemberVo(token);
+//        memberService.findByIdAndEmail(vo.getId(), vo.getEmail()).orElseThrow(
+//                ()->new ServletException("Invalid access 2")
+//        );
+//        chain.doFilter(request,response);
+//    }
+//}
