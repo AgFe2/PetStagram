cba81988af9b9823c1962bc2667b35cbcd4fa39d
chaeaw04 <chaeaw04@gmail.com>
2022-09-21 16:33:16 +0900

    fix : mainPage userData 구성 변경

--- src/main/front/src/pages/main/Main.js
+++ src/main/front/src/pages/main/Main.js
@@ -6,27 +6,93 @@ import styles from "../../styles/Contents.module.css";
 export default function Main() {
   const datas = [
     {
-      userId: "user01",
-      liked: 13,
-      comments: "23",
+      feed_id: "user01",
+      total_like_number: 13,
+      comment: [
+        {
+          comment_id: "user99",
+          comment_feed_id: "user01",
+          comment_context: "blahbalh",
+          commented_date: "20220921",
+          comment_writer: "user99",
+        },
+        {
+          comment_id: "user99",
+          comment_feed_id: "user01",
+          comment_context: "blahbalh",
+          commented_date: "20220921",
+          comment_writer: "user99",
+        },
+        {
+          comment_id: "user99",
+          comment_feed_id: "user01",
+          comment_context: "blahbalh",
+          commented_date: "20220921",
+          comment_writer: "user99",
+        },
+      ],
     },
     {
-      userId: "user02",
-      liked: "64",
-      comments: "189",
+      feed_id: "user02",
+      total_like_number: 75,
+      comment: [
+        {
+          comment_id: "user99",
+          comment_feed_id: "user01",
+          comment_context: "blahbalh",
+          commented_date: "20220921",
+          comment_writer: "user99",
+        },
+        {
+          comment_id: "user99",
+          comment_feed_id: "user01",
+          comment_context: "blahbalh",
+          commented_date: "20220921",
+          comment_writer: "user99",
+        },
+      ],
     },
     {
-      userId: "user03",
-      liked: "87",
-      comments: "65",
+      feed_id: "user03",
+      total_like_number: 564,
+      comment: [
+        {
+          comment_id: "user99",
+          comment_feed_id: "user01",
+          comment_context: "blahbalh",
+          commented_date: "20220921",
+          comment_writer: "user99",
+        },
+        {
+          comment_id: "user99",
+          comment_feed_id: "user01",
+          comment_context: "blahbalh",
+          commented_date: "20220921",
+          comment_writer: "user99",
+        },
+        {
+          comment_id: "user99",
+          comment_feed_id: "user01",
+          comment_context: "blahbalh",
+          commented_date: "20220921",
+          comment_writer: "user99",
+        },
+        {
+          comment_id: "user99",
+          comment_feed_id: "user01",
+          comment_context: "blahbalh",
+          commented_date: "20220921",
+          comment_writer: "user99",
+        },
+      ],
     },
   ];
 
   const contentsItem = datas.map((data) => (
     <Contents
-      userId={data.userId}
-      liked={data.liked}
-      comments={data.comments}
+      userId={data.feed_id}
+      liked={data.total_like_number}
+      comments={data.comment.length}
     ></Contents>
   ));
 
