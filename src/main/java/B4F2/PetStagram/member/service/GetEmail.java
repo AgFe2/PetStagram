57ad6b037da3f4c9c4b63a125c3220448aaeb71a
DYS <kcood92@gmail.com>
2022-09-22 17:03:52 +0900

    feat: getEmail메소드 별도 믈래스로 구현

--- src/main/java/B4F2/PetStagram/member/service/GetEmail.java
+++ src/main/java/B4F2/PetStagram/member/service/GetEmail.java
@@ -1,7 +1,11 @@
 package B4F2.PetStagram.member.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import javax.servlet.http.HttpServletRequest;
 
+@Service
 public class GetEmail {
 
     public Object getEmailFromToken(HttpServletRequest request) {
