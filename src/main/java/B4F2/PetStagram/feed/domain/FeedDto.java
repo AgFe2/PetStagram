bde539684bbb1d8b0be0c5122f0135376791ae55
pjy3012 <ppp0868@naver.com>
2022-09-23 13:53:52 +0900

    [004]-Add_FeedFunction

---
+++ src/main/java/B4F2/PetStagram/feed/domain/FeedDto.java
@@ -0,0 +1,39 @@
+package B4F2.PetStagram.feed.domain;
+
+import B4F2.PetStagram.feed.entity.FeedEntity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FeedDto {
+
+    private Long feedId;
+    private String mainText;
+    private String userId;
+
+    private LocalDateTime updateDit;
+
+    private Long like;
+
+//    차후 comment 파트와 함께 merge 되면 추가
+//    @OneToMany
+//    private List<CommentEntity> comments;
+
+    // 업로드 사진
+
+    public static FeedDto fromEntity(FeedEntity feed) {
+
+        return  FeedDto.builder()
+                .feedId(feed.getFeedId())
+                .mainText(feed.getMainText())
+                .like(feed.getLike())
+                .updateDit(feed.getUpdateDit())
+                .userId(feed.getUserId())
+                .build();
+    }
+}
