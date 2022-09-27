d6abb736aaabb99d193bd48c24e5f9d84d7c0e15
chaeaw04 <chaeaw04@gmail.com>
2022-09-27 18:17:07 +0900

    feat : contentsModal 작성

--- src/main/front/src/components/Modal/ContentModal.js
+++ src/main/front/src/components/Modal/ContentModal.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 import ItemUser from "../Contents/ItemUser";
-import InfoParagraph from "../ContentsInfo/InfoParagraph";
+import Paragraph from "../ContentsInfo/Paragraph";
+import Comments from "../Contents/Comments";
+import Liked from "../ContentsInfo/Liked";
 
-import styles from "../../styles/ContentDetail.module.css";
+import styles from "../../styles/ContentModal.module.css";
 
 function ContentModal(props) {
   const { onClick } = props;
@@ -13,10 +15,30 @@ function ContentModal(props) {
         ✖
       </button>
       <div className={styles.body} onClick={(e) => e.stopPropagation()}>
-        <div className={styles.mainPic}>사진</div>
-        <div>
-          <ItemUser />
-          <InfoParagraph />
+        <div className={styles.pic}>사진</div>
+        <div className={styles.info}>
+          <div className={styles.infoTop}>
+            <ItemUser userId={"userId"} />
+          </div>
+          <div className={styles.main}>
+            <div className={styles.scroll}>
+              <Paragraph />
+              <Comments />
+            </div>
+            <div className={`${styles.likedBox} ${styles.infoBottom}`}>
+              <Liked />
+            </div>
+            <div className={styles.inputForm}>
+              <input
+                type="text"
+                placeholder="댓글 달기..."
+                className={styles.inputComment}
+              />
+              <button type="button" className={styles.inputButton}>
+                게시
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
