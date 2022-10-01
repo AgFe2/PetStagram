ccc46c0f13e402d959c7b4a319659c39f59f691e
chaeaw04 <chaeaw04@gmail.com>
2022-10-01 16:16:56 +0900

    feat : 페이지별 gallery 관련 axios

--- src/main/front/src/pages/my/My.js
+++ src/main/front/src/pages/my/My.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import Header from "../../components/Header/Header";
 import Info from "../../components/Info/Info";
@@ -7,11 +8,22 @@ import Gallery from "../../components/post/Gallery";
 import styles from "../../styles/My.module.css";
 
 function My() {
+  const [gallery, setGallery] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/board/myList")
+      .then((res) => {
+        console.log(res.data);
+        setGallery(res.data);
+      })
+      .catch((err) => console.log(err));
+  });
   return (
     <>
       <Header />
       <Info />
-      <Gallery />
+      <Gallery data={gallery} />
     </>
   );
 }
--- src/main/front/src/pages/top/Top.js
+++ src/main/front/src/pages/top/Top.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import Header from "../../components/Header/Header";
 import Gallery from "../../components/post/Gallery";
@@ -6,11 +7,23 @@ import Gallery from "../../components/post/Gallery";
 import styles from "../../styles/Top.module.css";
 
 function Top(props) {
+  const [gallery, setGallery] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/board/bestList")
+      .then((res) => {
+        console.log(res.data);
+        setGallery(res.data);
+      })
+      .catch((err) => console.log(err));
+  });
+
   return (
     <>
       <Header />
       <div className={styles.galleryWrap}>
-        <Gallery />
+        <Gallery data={gallery} />
       </div>
     </>
   );
