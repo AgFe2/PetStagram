2978091e81721465bd3e6d7993216e79ac08cb98
chaeaw04 <chaeaw04@gmail.com>
2022-10-02 22:15:31 +0900

    feat : main axios

--- src/main/front/src/pages/main/Main.js
+++ src/main/front/src/pages/main/Main.js
@@ -1,95 +1,95 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "../../components/Header/Header";
 import Contents from "../../components/Contents/Contents";
 import styles from "../../styles/Contents.module.css";
 import axios from "axios";
 export default function Main() {
-  const datas = [
-    {
-      feed_id: "user01",
-      total_like_number: 13,
-      comment: [
-        {
-          comment_id: "user99",
-          comment_feed_id: "user01",
-          comment_context: "blahbalh",
-          commented_date: "20220921",
-          comment_writer: "user99",
-        },
-        {
-          comment_id: "user99",
-          comment_feed_id: "user01",
-          comment_context: "blahbalh",
-          commented_date: "20220921",
-          comment_writer: "user99",
-        },
-        {
-          comment_id: "user99",
-          comment_feed_id: "user01",
-          comment_context: "blahbalh",
-          commented_date: "20220921",
-          comment_writer: "user99",
-        },
-      ],
-    },
-    {
-      feed_id: "user02",
-      total_like_number: 75,
-      comment: [
-        {
-          comment_id: "user99",
-          comment_feed_id: "user01",
-          comment_context: "blahbalh",
-          commented_date: "20220921",
-          comment_writer: "user99",
-        },
-        {
-          comment_id: "user99",
-          comment_feed_id: "user01",
-          comment_context: "blahbalh",
-          commented_date: "20220921",
-          comment_writer: "user99",
-        },
-      ],
-    },
-    {
-      feed_id: "user03",
-      total_like_number: 564,
-      comment: [
-        {
-          comment_id: "user99",
-          comment_feed_id: "user01",
-          comment_context: "blahbalh",
-          commented_date: "20220921",
-          comment_writer: "user99",
-        },
-        {
-          comment_id: "user99",
-          comment_feed_id: "user01",
-          comment_context: "blahbalh",
-          commented_date: "20220921",
-          comment_writer: "user99",
-        },
-        {
-          comment_id: "user99",
-          comment_feed_id: "user01",
-          comment_context: "blahbalh",
-          commented_date: "20220921",
-          comment_writer: "user99",
-        },
-        {
-          comment_id: "user99",
-          comment_feed_id: "user01",
-          comment_context: "blahbalh",
-          commented_date: "20220921",
-          comment_writer: "user99",
-        },
-      ],
-    },
-  ];
+  // const datas = [
+  //   {
+  //     feed_id: "user01",
+  //     total_like_number: 13,
+  //     comment: [
+  //       {
+  //         comment_id: "user99",
+  //         comment_feed_id: "user01",
+  //         comment_context: "blahbalh",
+  //         commented_date: "20220921",
+  //         comment_writer: "user99",
+  //       },
+  //       {
+  //         comment_id: "user99",
+  //         comment_feed_id: "user01",
+  //         comment_context: "blahbalh",
+  //         commented_date: "20220921",
+  //         comment_writer: "user99",
+  //       },
+  //       {
+  //         comment_id: "user99",
+  //         comment_feed_id: "user01",
+  //         comment_context: "blahbalh",
+  //         commented_date: "20220921",
+  //         comment_writer: "user99",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     feed_id: "user02",
+  //     total_like_number: 75,
+  //     comment: [
+  //       {
+  //         comment_id: "user99",
+  //         comment_feed_id: "user01",
+  //         comment_context: "blahbalh",
+  //         commented_date: "20220921",
+  //         comment_writer: "user99",
+  //       },
+  //       {
+  //         comment_id: "user99",
+  //         comment_feed_id: "user01",
+  //         comment_context: "blahbalh",
+  //         commented_date: "20220921",
+  //         comment_writer: "user99",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     feed_id: "user03",
+  //     total_like_number: 564,
+  //     comment: [
+  //       {
+  //         comment_id: "user99",
+  //         comment_feed_id: "user01",
+  //         comment_context: "blahbalh",
+  //         commented_date: "20220921",
+  //         comment_writer: "user99",
+  //       },
+  //       {
+  //         comment_id: "user99",
+  //         comment_feed_id: "user01",
+  //         comment_context: "blahbalh",
+  //         commented_date: "20220921",
+  //         comment_writer: "user99",
+  //       },
+  //       {
+  //         comment_id: "user99",
+  //         comment_feed_id: "user01",
+  //         comment_context: "blahbalh",
+  //         commented_date: "20220921",
+  //         comment_writer: "user99",
+  //       },
+  //       {
+  //         comment_id: "user99",
+  //         comment_feed_id: "user01",
+  //         comment_context: "blahbalh",
+  //         commented_date: "20220921",
+  //         comment_writer: "user99",
+  //       },
+  //     ],
+  //   },
+  // ];
 
-  // const [contents, setContents] = useState([]);
+  const [contents, setContents] = useState([]);
 
   // useEffect(() => {
   //   axios
@@ -111,7 +111,10 @@ export default function Main() {
           Authorization: "Bearer " + localStorage.getItem("JWT"),
         },
       })
-      .then((res) => console.log(res))
+      .then((res) => {
+        console.log(res);
+        setContents(res.data);
+      })
       .then((json) => alert(json));
   }, []);
 
@@ -120,12 +123,12 @@ export default function Main() {
       <Header />
       <div className="container">
         <div className={styles.contentsGroup}>
-          {datas.map((data) => (
+          {contents.map((item) => (
             <Contents
-              userId={data.feed_id}
-              liked={data.total_like_number}
-              comments={data.comment.length}
-              key={data.feed_id}
+              userId={item.feed_id}
+              liked={item.total_like_number}
+              comments={item.comment.length}
+              key={item.feed_id}
             />
           ))}
         </div>
