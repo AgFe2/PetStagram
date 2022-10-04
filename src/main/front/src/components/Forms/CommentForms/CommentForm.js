1c6e64a721668c536b1598572cde05511ecae041
SungJin Yang <sungjinahim@gmail.com>
2022-10-04 17:21:28 +0900

    댓글 저장부분 컴퍼넌트화 시킴

--- src/main/front/src/components/Forms/CommentForms/CommentForm.js
+++ src/main/front/src/components/Forms/CommentForms/CommentForm.js
@@ -4,7 +4,7 @@ import styles from "../../../styles/ContentModal.module.css";
 
 const CommentForm = ({context}) => {
     const [commentValue, setCommentValue] = useState("");
-    
+    const [lists, setLists] = useState([])
 
     const onChangeComment = (e) => {
         console.log(typeof e.target.value);
@@ -37,7 +37,7 @@ const CommentForm = ({context}) => {
                     axios.get(`http://localhost:8080/feed/show-comments?feedId=${feed_Id}`)
                     .then((response) => {
                         console.log(response.data)
-                        context = response.data
+                        setLists(response.data)
                     })
                 })
 
@@ -45,15 +45,28 @@ const CommentForm = ({context}) => {
         catch (e) { console.log(e) };
     };
 
-    console.log(context)
+    const CommentList = () =>{
+        return(
+            <>
+                {lists.map((list) =>{
+                    <div>
+                        <div>{list.email}</div>
+                        <div>{list.comment}</div>
+                        <div>{list.createdAt}</div>
+                    </div>
+                })}
+            </>
+        )
+    }
     return (
-        <>  
+        <div>
+            <CommentList />
             <form className={styles.inputForm} onSubmit={addComment}>
                 <input
                     type="text"
                     placeholder="댓글 달기..."
                     className={styles.inputComment}
-                    value={context}
+                    value={commentValue}
                     onChange={onChangeComment}
                 />
                 {commentValue.length > 0 ? (
@@ -69,7 +82,7 @@ const CommentForm = ({context}) => {
                     </button>
                 )}
             </form>
-        </>
+        </div>
     );
 };
 
