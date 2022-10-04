f85f097d51ac874356b0462b81cc536e50767927
SungJin Yang <sungjinahim@gmail.com>
2022-10-04 17:15:09 +0900

    댓글 저장부분 컴퍼넌트화 시킴

--- src/main/front/src/components/Forms/CommentForms/CommentForm.js
+++ src/main/front/src/components/Forms/CommentForms/CommentForm.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import axios from "axios";
 import styles from "../../../styles/ContentModal.module.css";
 
-const CommentForm = ({comment}) => {
+const CommentForm = ({context}) => {
     const [commentValue, setCommentValue] = useState("");
+    
 
     const onChangeComment = (e) => {
         console.log(typeof e.target.value);
@@ -33,22 +34,26 @@ const CommentForm = ({comment}) => {
                 .then((res) => {
                     setCommentValue("");
                     console.log(res);
-                    axios.get(`http://localhost:8080/feed/show-comments?feedId=${feed_Id}`).then((data) => console.log(data))
+                    axios.get(`http://localhost:8080/feed/show-comments?feedId=${feed_Id}`)
+                    .then((response) => {
+                        console.log(response.data)
+                        context = response.data
+                    })
                 })
 
         }
         catch (e) { console.log(e) };
     };
 
-    console.log(commentValue)
+    console.log(context)
     return (
-        <>
+        <>  
             <form className={styles.inputForm} onSubmit={addComment}>
                 <input
                     type="text"
                     placeholder="댓글 달기..."
                     className={styles.inputComment}
-                    value={commentValue}
+                    value={context}
                     onChange={onChangeComment}
                 />
                 {commentValue.length > 0 ? (
