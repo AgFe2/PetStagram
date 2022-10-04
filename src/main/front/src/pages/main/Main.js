7737b9ab7ccdf2346e70b34feef9bb8a9b430643
SungJin Yang <sungjinahim@gmail.com>
2022-10-04 13:25:50 +0900

    임시피드 추가

--- src/main/front/src/pages/main/Main.js
+++ src/main/front/src/pages/main/Main.js
@@ -4,39 +4,40 @@ import Header from "../../components/Header/Header";
 import Contents from "../../components/Contents/Contents";
 import styles from "../../styles/Contents.module.css";
 import axios from "axios";
+import feed from '../../data/feed.json'
 export default function Main() {
   const [contents, setContents] = useState([]);
   const [commentsLength, setCommentsLength] = useState();
 
   // 토큰 및 보드
-  useEffect(() => {
-    axios
-      .get("/board/followList", {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + localStorage.getItem("JWT"),
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-        setContents(res.data);
-      })
-      .then((json) => alert(json));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("/board/followList", {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: "Bearer " + localStorage.getItem("JWT"),
+  //       },
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       setContents(res.data);
+  //     })
+  //     .then((json) => alert(json));
+  // }, []);
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/feed/show-comments", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        params: {
-          feedId: contents.map((item) => item.feedId),
-        },
-      })
-      .then((res) => setCommentsLength(res.data.content.length))
-      .catch((e) => console.log(e));
-  });
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:8080/feed/show-comments", {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       params: {
+  //         feedId: contents.map((item) => item.feedId),
+  //       },
+  //     })
+  //     .then((res) => setCommentsLength(res.data.content.length))
+  //     .catch((e) => console.log(e));
+  // });
 
   // comments 갯수는 feedId를 통해서 comment에서 가져와야함.......
   return (
@@ -44,7 +45,7 @@ export default function Main() {
       <Header />
       <div className="container">
         <div className={styles.contentsGroup}>
-          {contents.map((item) => (
+          {feed.map((item) => (
             <Contents
               userId={item.userId}
               liked={item.likeCnt}
